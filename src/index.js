10a14d98c72b622772a8f1bd057f53ee0ff4d3cd
pathas <pathas@naver.com>
2020-03-10 15:51:34 +0900

    SetUp final

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "Components/App";
-import { injectGlobal } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import "./typography";
 
-injectGlobal`
+const globalStyle = createGlobalStyle`
 ${reset};
 body{
     background-color:#ecf0f1;
 }`;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.Fragment>
+    <globalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
